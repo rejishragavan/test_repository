100039467ca5c1ea79d45b4f12e3f4c4e87dacfe
rejishragvan <ragavanrejish@gmail.com>
2023-10-09 05:42:02 +0530

    first commit

---
+++ Name.py
@@ -0,0 +1,3 @@
+count=[1,2,3,4,5]
+for i in count:
+	print("RejishRagavan")
---
+++ Name2.py
@@ -0,0 +1,8 @@
+
+def display_name():
+    count = [1,2,3,4,5]
+    for i in count:
+        print("RejishRagavan")
+        
+if __name__=="__main__":
+    display_name()
